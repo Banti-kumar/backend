ab15c20a3e41da50f0effde9353aa6eca1bf7e94
Bantikumar <108114874+Banti-kumar@users.noreply.github.com>
2025-01-17 13:17:51 +0530

    Update index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,1 +1,39 @@
-console.log("hello");
+import { V2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY_NAME,
+  api_secret: process.env.API_KEY_SECRET,
+});
+
+const uploadOnCloudinary = async (localfilePath) => {
+  try {
+    if (!localfilePath) return null;
+    // file upload on the cloudnary successfully
+    const res = await cloudinary.uploader.upload(localfilePath, {
+      resource_type: "auto",
+    });
+    console.log("file upload on the cloudnary successfully", res);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localfilePath); // remove the locally saved temporary file as the upload operation got failed
+    console.log("file is not upload on the cloudnary", error);
+    return null;
+  }
+};
+
+export default uploadOnCloudinary;
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+ export default const upload = multer({ storage })
