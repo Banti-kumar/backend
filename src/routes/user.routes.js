9f834c6bfe0eeebd933a2826754e1ae58b8c4579
Bantikumar <108114874+Banti-kumar@users.noreply.github.com>
2025-01-21 12:08:46 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,7 @@ import {
   userLogin,
   userLogout,
   userRegister,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 
 const router = Router();
@@ -23,5 +24,6 @@ router.route("/login").post(userLogin);
 // SET PROTECT MIDDLEWARE
 
 router.route("/logout").post(protect, userLogout);
+router.route("/refreshtoken").post(refreshAccessToken);
 
 export default router; // Export the userRouter for use in the main app file
